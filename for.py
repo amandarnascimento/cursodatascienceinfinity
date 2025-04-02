0450510feacffee52f5562f302586c355f4ae302
Amanda Nascimento <116469203+amandarnascimento@users.noreply.github.com>
2025-04-02 19:48:36 -0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,45 @@
+#Crie um programa que imprima os números pares de 2 a 10.
+
+# for i in range(2, 11, 2):
+#     print(i)
+
+
+# for i in range(3, 200):
+#     if i % 2 == 0:
+#         print(i)
+
+#Utilize um loop for para calcular o produto dos números de 1a 5.
+
+
+# multiplicacao = 1
+# for i in range(1, 6):
+#     multiplicacao *= i
+# print(multiplicacao)
+
+
+# resultado = 1
+# for i in range(1, 6):
+#     print(f'{resultado} x {i} = {resultado * 1}')
+#     resultado *= i
+
+
+''' for com break e continue'''
+
+
+# for i in range(20):
+#     print(i)
+#     if i ==15:
+#         print('tchau')
+#         break
+
+
+'''Crie uma programa que solicite 10 números para o usuário.
+O programa deve somar todos os números múltiplos de 6
+digitados. Se a soma for igual ou maior que 30, o programa
+
+dever parar e mostrar o resultado da soma.'''
+
+
+
+'''Utilize um loop for para imprimir os números de 1 a 20,
+pulando os múltiplos de 3.'''
